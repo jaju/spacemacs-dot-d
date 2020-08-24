874bad06273d22bf862129f6fe9253f7f419ea66
Ravindra R. Jaju <jaju@msync.org>
2020-08-24 13:51:28 +0530

    Fixed emacs-el linting advice

--- hugo.el
+++ hugo.el
@@ -1,5 +1,10 @@
+;;; hugo.el --- Hugo configuration
+;;; Commentary:
+;;; Code:
 (require 'easy-hugo)
 (setq easy-hugo-basedir "~/Projects/hugo-blog/")
 (setq easy-hugo-url "https://blog.msync.org")
 (setq easy-hugo-default-ext ".org")
 (define-key global-map (kbd "C-c C-e") 'easy-hugo)
+(provide 'hugo)
+;;; hugo.el ends here
